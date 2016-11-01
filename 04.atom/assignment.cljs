833c6b1534d1976b64b06c3b5a2567026f6866f4
Roman Liutikov <roman01la@romanliutikov.com>
2016-11-01 17:07:57 +0200

    add 4th lesson

---
+++ 04.atom/assignment.cljs
@@ -0,0 +1,6 @@
+;; Given a collection of dirty dishes
+;; create a process of washing dishes and
+;; stacking washed dishes onto another collection.
+;; Log remaining dirty dishes.
+
+(def dirty-dishes [:dish1 :dish2 :dish3 :dish4 :dish5])
