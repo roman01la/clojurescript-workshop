7d3e2ca8e3173b7b2ebaee4591eac6b974edb85b
Roman Liutikov <roman01la@romanliutikov.com>
2016-11-16 14:55:35 +0200

    update 13 lesson with macro requires

--- 13.namespaces/app/components/base.cljs
+++ 13.namespaces/app/components/base.cljs
@@ -1,3 +1,7 @@
-(ns app.components.base
+(ns app.components.base ;; <-- one-to-one mapping between a namespace name
+                        ;;     and the path of the file with respect to source root
   (:require [app.app-utils :as autils] ;; import * as autils from "app.app-utils"
-            [app.components.button :refer [button]])) ;; import { button } from "app.components.button"
+            [app.components.button :refer [button]] ;; import { button } from "app.components.button"
+            [my.macros :refer-macros [somem]] ;; ClojureScript <1.8
+            [my.macros :refer [somem]]) ;; ClojureScript 1.9>
+  (:require-macros [my.macros :as my])) ;; ClojureScript <1.8
